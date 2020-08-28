b86aa0358a41c0b82a49ec8e541011592d6ac9fa
vinay_kuresi <vinaykumar7655@gmail.com>
2020-08-28 18:16:50 +0530

    Documentations

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 
 // MongoDb uri for db transactions
 const MongoClient = require('mongodb').MongoClient;
-module.exports = new MongoClient("mongodb+srv://backendconcoxdeveloper:V3jUV7QXqEoAtnhy@cluster0-zhjde.mongodb.net", 
+module.exports = new MongoClient("Connect your DB URl", 
     {
     useNewUrlParser: true,
     useUnifiedTopology: true
